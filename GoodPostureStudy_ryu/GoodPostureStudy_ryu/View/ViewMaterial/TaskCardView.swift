d649192de989cc35a2928c86af78124580a11bc8
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-05-11 02:42:24 +0900

    強制アンラップの削除

--- GoodPostureStudy_ryu/GoodPostureStudy_ryu/View/ViewMaterial/TaskCardView.swift
+++ GoodPostureStudy_ryu/GoodPostureStudy_ryu/View/ViewMaterial/TaskCardView.swift
@@ -11,6 +11,14 @@ struct TaskCardView: View {
     @Binding var selectedTask: Task?
     // タスク
     @State var task: Task
+    // タスクの色
+    @State var taskColor: Color = .blue
+    // タスク内容
+    @State var taskContent: String = ""
+    // 開始時間
+    @State var startTime: Date = Date()
+    // 終了時間
+    @State var endTime: Date = Date()
 
     var body: some View {
         HStack(spacing: 30) {
@@ -20,18 +28,18 @@ struct TaskCardView: View {
                 .padding(8)
                 .foregroundColor(.white)
                 .frame(width: 45, height: 45)
-                .background(Color(taskColorName: Color.TaskColorNames(rawValue: task.color!)!).opacity(0.5))
+                .background(taskColor.opacity(0.5))
                 .cornerRadius(8)
-                .shadow(color: Color(taskColorName: Color.TaskColorNames(rawValue: task.color!)!), radius: 5, x: 3, y: 3)
+                .shadow(color: taskColor, radius: 5, x: 3, y: 3)
                 .shadow(color: .white.opacity(0.5), radius: 5, x: -3, y: -3)
                 .padding(.leading, 25)
 
             VStack {
-                Text(task.task!)
+                Text(taskContent)
                     .font(.system(size: 18))
                     .bold()
 
-                Text("\(task.startTime!.formattedTimeString())〜\(task.endTime!.formattedTimeString())")
+                Text("\(startTime.formattedTimeString())〜\(endTime.formattedTimeString())")
                     .font(.system(size: 15))
             }
             .frame(width: 130)
@@ -50,6 +58,20 @@ struct TaskCardView: View {
             }
             .padding(.trailing, 25)
             .disabled(task.isDone)
+        }
+        .onAppear {
+            // タスクの色をTaskデータで更新
+            guard let color = task.color else { return }
+            guard let colorName = Color.TaskColorNames(rawValue: color) else { return }
+            taskColor = Color(taskColorName: colorName)
+            // タスク内容をTaskデータで更新
+            guard let content = task.task else { return }
+            taskContent = content
+            // タスクの開始時間と終了時間をTaskデータで更新
+            guard let start = task.startTime else { return }
+            startTime = start
+            guard let end = task.endTime else { return }
+            endTime = end
         }
     }
 }
